d2fdbd00f9ca5fbe0a7e9e2ec2a9293af1b1ad43
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2025-04-24 00:40:25 -0600

    generar arreglos random

--- tareas_programadas/tp1/source/Ordenador.cpp
+++ tareas_programadas/tp1/source/Ordenador.cpp
@@ -3,23 +3,51 @@
 #include "Ordenador.hpp"
 
 void Ordenador::ordenamientoPorSeleccion(uint32_t *A, uint32_t n) const{
-    // va escogiendo el elemento más pequeño y poniéndolo en la posición i
-    // recorre el arreglo A
-    for (int i = 0; i < n - 1; i++) {
+    // Va escogiendo el elemento más pequeño y poniéndolo en la posición i
+
+    // Empieza a contar la duración
+    auto inicio = std::chrono::high_resolution_clock::now();
+
+    // Recorre el arreglo A
+    for (uint32_t i = 0; i < n - 1; i++) {
         // m es el índice del elemento mínimo
         int m = i;
-        // busca el elemento mínimo en el resto del arreglo
-        for (int j = i + 1; j < n; j++) {
+        // Busca el elemento mínimo en el resto del arreglo
+        for (u_int32_t j = i + 1; j < n; j++) {
             if (A[j] < A[m]) {
                 m = j;
             }
         }
-        // intercambia el elemento mínimo con el elemento i
+        // Intercambia el elemento mínimo con el elemento i
         std::swap(A[i], A[m]);
     }
-    std::cout << "Ordenamiento por seleccion: " << std::endl;
-    imprimirArreglo(A, n);
+
+    // Detener el cronómetro
+    auto fin = std::chrono::high_resolution_clock::now();
+    // Calcular la duración
+    auto duracion = std::chrono::duration_cast<std::chrono::milliseconds>(fin - inicio).count();
+
+    std::cout << "Ordenamiento por seleccion. Tamaño: " << n << std::endl;
+    
     estaOrdenado(A, n, "Ordenamiento por seleccion");
+    // Imprimir el tiempo de ejecución
+    std::cout << duracion << " milisegundos" << std::endl;
+}
+
+void Ordenador::ordenamientoPorInserccion(uint32_t *A, uint32_t n) const{
+    //TODO
+}
+void Ordenador::ordenamientoPorMezcla(uint32_t *A, uint32_t n) const{
+    //TODO
+}
+void Ordenador::ordenamientoPorMonticulos(uint32_t *A, uint32_t n) const{
+    //TODO
+}
+void Ordenador::ordenamientoRapido(uint32_t *A, uint32_t n) const{
+    //TODO
+}
+void Ordenador::ordenamientoPorResiduos(uint32_t *A, uint32_t n) const{
+    //TODO
 }
 
 void Ordenador::imprimirArreglo(uint32_t *A, uint32_t n) const{
@@ -32,10 +60,10 @@ void Ordenador::imprimirArreglo(uint32_t *A, uint32_t n) const{
 bool Ordenador::estaOrdenado(uint32_t *A, uint32_t n, const char* nombre) const{
     for (uint32_t i = 1; i < n; i++) {
         if (A[i] < A[i - 1]) {
-            std::cout << nombre << " no ordenó." << std::endl;
+            std::cout << "No ordenó en " << std::endl;
             return false;
         }
     }
-    std::cout << nombre << " ordenó bien." << std::endl;
+    std::cout << "Ordenó bien en " << std::endl;
     return true;
 }
--- tareas_programadas/tp1/source/main.cpp
+++ tareas_programadas/tp1/source/main.cpp
@@ -11,23 +11,53 @@ uint32_t* copiarArreglo(uint32_t *A, uint32_t n) {
     return B;
 }
 
+uint32_t* generarArregloAleatorio(uint32_t n) {
+    uint32_t* arreglo = new uint32_t[n];
+
+    // Configurar el generador de números aleatorios
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // Los números van de 0 a UINT32_MAX
+    std::uniform_int_distribution<uint32_t> distribucion(0, UINT32_MAX);
+
+    // Llenar el arreglo con números aleatorios
+    for (uint32_t i = 0; i < n; i++) {
+        arreglo[i] = distribucion(gen);
+    }
+
+    return arreglo;
+}
+
 int main() {
     const Ordenador ordenador;
 
-    // TODO: crear arreglos de mil, 10mil, 100mil y 1 millón de elementos
-    uint32_t arr[] = {64, 34, 2, 12, 0, 11, 9};
+    // Generar arreglos aleatorios
+    uint32_t* mil = generarArregloAleatorio(1000);
+    uint32_t* diezMil = generarArregloAleatorio(10000);
+    uint32_t* cienMil = generarArregloAleatorio(100000);
+    uint32_t* unMillon = generarArregloAleatorio(1000000);
 
-    // Crear una copia del arreglo
-    uint32_t* copia = copiarArreglo(arr, 7);
+    // Crear una copias de los arreglos
+    uint32_t* copia_mil = copiarArreglo(mil, 1000);
+    uint32_t* copia_diezMil = copiarArreglo(diezMil, 10000);
+    uint32_t* copia_cienMil = copiarArreglo(cienMil, 100000);
+    uint32_t* copia_unMillon = copiarArreglo(unMillon, 1000000);
 
-    ordenador.ordenamientoPorSeleccion(copia, 7);
+    // Ordenar por selección
+    ordenador.ordenamientoPorSeleccion(copia_mil, 1000);
+    ordenador.ordenamientoPorSeleccion(copia_diezMil, 10000);
+    ordenador.ordenamientoPorSeleccion(copia_cienMil, 100000);
+    ordenador.ordenamientoPorSeleccion(copia_unMillon, 1000000);
 
-    for (uint32_t i = 0; i < 7; i++) {
-        std::cout << arr[i] << " ";
-    }
-
-    // Liberar la memoria de la copia
-    delete[] copia;
+    // Liberar la memoria
+    delete[] mil;
+    delete[] diezMil;
+    delete[] cienMil;
+    delete[] unMillon;
+    delete[] copia_mil;
+    delete[] copia_diezMil;
+    delete[] copia_cienMil;
+    delete[] copia_unMillon;     
 
     return 0;
 }
